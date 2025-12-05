74cc29d59388e187a20ae126bda1372c8a31a701
janniktaulan <janniktn1999@hotmail.com>
2025-12-05 14:15:46 +0100

    code reduction and fix for service reload

--- tz-lego-combined.sh
+++ tz-lego-combined.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 function cronjob() {
     if cron="true"; then
-        read -n 1 -p "Do you want to create a cronjob for automatic renewal? (y/n): " cronjob_choice
-        echo ""
+        read -n 1 -p "Do you want to create a cronjob for automatic renewal? (y/n): " cronjob_choice && echo ""
         if [[ "$cronjob_choice" == "y" ]]; then
-            renewal="yes"
+            renewal="yes" 
             echo "Selecting automatic renewal"
             job='0 6 * * * /etc/tz-bot/scripts/renewal.sh 2> /dev/null' 
             (crontab -l 2>/dev/null | grep -Fxq -- "$job") || (crontab -l 2>/dev/null; printf '%s\n' "$job") | crontab - 
@@ -29,7 +28,7 @@ function auto_reload() {
             reload_command="sudo systemctl reload $server" && echo ""
             ;;
         2)
-            reload_command="sudo service reload $server" && echo ""
+            reload_command="sudo service $server reload" && echo ""
             ;;
         3)
             reload_command="sudo /etc/init.d/nginx reload" && echo ""
@@ -45,24 +44,20 @@ function auto_reload() {
             exit 1
             ;;
     esac
-    echo ""
-    echo "Attempting to reload server using command: $reload_command"
+    echo "" && echo "Attempting to reload server using command: $reload_command"
     if sudo $reload_command; then
-        echo "Web server reloaded successfully."
-        echo "$reload_command" >> /etc/tz-bot/scripts/renewal_hook.sh
+        echo "Web server reloaded successfully." && echo "$reload_command" >> /etc/tz-bot/scripts/renewal_hook.sh
         if grep -q "$reload_command" "/etc/tz-bot/scripts/renewal_hook.sh"; then
             sudo sed -i.bak "\#$reload_command#d" /etc/tz-bot/scripts/renewal_hook.sh
             echo "$reload_command" >> /etc/tz-bot/scripts/renewal_hook.sh
         fi
     else
-        echo "Failed to reload using: '$reload_command'"
-        echo ""
+        echo "Failed to reload using: '$reload_command'" && echo ""
         read -n 1 -p "Would you like to try another reload command? (y/n): " retry_reload
         if [[ "$retry_reload" = "y" ]]; then
             auto_reload
         else
-            echo ""
-            echo "Automatic server reloading cancelled."
+            echo "" && echo "Automatic server reloading cancelled."
         fi
     fi
 }
@@ -87,21 +82,15 @@ function upkeep() {
         read -n 1 -p "New version found: $remote_version. Do you want to update? (y/n): " update_choice
         if [[ "$update_choice" == "y" ]]; then
             curl -fsSL "https://raw.githubusercontent.com/Trustzone-A-S/TZ-bot-lego/code-reduction-testing/tz-lego-combined.sh" \
-        -o "$SCRIPT_PATH.tmp" || exit 1
-
-        mv "$SCRIPT_PATH.tmp" "$SCRIPT_PATH"
-        chmod +x "$SCRIPT_PATH"
-            echo -e "\nUpdate done! Please run tz-bot again."
+            -o "$SCRIPT_PATH.tmp" || exit 1
+            mv "$SCRIPT_PATH.tmp" "$SCRIPT_PATH" && chmod +x "$SCRIPT_PATH" && echo -e "\nUpdate done! Please run tz-bot again."
             exit 0
         fi
     fi
-
     if ! command -v tz-bot >/dev/null 2>&1; then
         sudo mkdir -p /usr/local/bin
         if sudo mv /tmp/tz-bot /usr/local/bin/tz-bot; then
-            sudo chmod +x /usr/local/bin/tz-bot
-            sudo mkdir -p /etc/tz-bot
-            echo "TZ-Bot has been installed successfully. You can now run it using the command 'sudo tz-bot'"
+            sudo chmod +x /usr/local/bin/tz-bot && sudo mkdir -p /etc/tz-bot && echo "TZ-Bot has been installed successfully. You can now run it using the command 'sudo tz-bot'"
             exit
         else
             echo -e "\nInstallation failed."
@@ -125,16 +114,14 @@ function upkeep() {
     fi
     cron="true"
     if ! command -v crontab >/dev/null 2>&1; then
-        echo "---------WARNING---------"
-        echo "Crontab is NOT installed."
-        echo "Automatic renewal via cronjobs will not be available."
+        echo "---------WARNING---------" && echo "Crontab is NOT installed." && echo "Automatic renewal via cronjobs will not be available."
         read -n 1 -p "Do you want TZ-bot to try installing cron/crontab? (y/n): " install_cron
         if [[ "$install_cron" == "y" ]]; then
             echo -e "\nInstalling cron..."
             sudo apt-get update && sudo apt-get install cron -y
                 if ! command -v crontab >/dev/null 2>&1; then
-                echo -e "\nCrontab installation failed. Please install cron/crontab manually."
-                exit 1
+                    echo -e "\nCrontab installation failed. Please install cron/crontab manually."
+                    exit 1
                 fi
         else
             echo -e "\nEntering manual renewal mode."
@@ -142,45 +129,35 @@ function upkeep() {
         fi
     fi
     mkdir -p /etc/tz-bot/scripts/ && mkdir -p /etc/tz-bot/certs/
-    
     if ! [ -e "/etc/tz-bot/scripts/.ca" ] ; then
-        touch /etc/tz-bot/scripts/.ca
-        sudo echo "selected_ca=https://emea.acme.atlas.globalsign.com/directory" > /etc/tz-bot/scripts/.ca
+        touch /etc/tz-bot/scripts/.ca && sudo echo "selected_ca=https://emea.acme.atlas.globalsign.com/directory" > /etc/tz-bot/scripts/.ca
     fi
-
     if ! [ -e "/etc/tz-bot/scripts/storage" ] ; then
         touch /etc/tz-bot/scripts/storage
     fi
-    
     if ! [ -e "/etc/tz-bot/scripts/.azure_credentials" ] ; then
         touch /etc/tz-bot/scripts/.azure_credentials
     fi
     if ! [ -e "/etc/tz-bot/scripts/.aws_credentials" ] ; then
         touch /etc/tz-bot/scripts/.aws_credentials
     fi
-
     if ! [ -e "/etc/tz-bot/scripts/.cloudflare_credentials" ] ; then
         touch /etc/tz-bot/scripts/.cloudflare_credentials
     fi
-
     if ! [ -e "/etc/tz-bot/scripts/.domeneshop_credentials" ] ; then
         touch /etc/tz-bot/scripts/.domeneshop_credentials
     fi
-
     if ! [ -e "/etc/tz-bot/scripts/.infoblox_credentials" ] ; then
         touch /etc/tz-bot/scripts/.infoblox_credentials
     fi
-
     if ! [ -e "/etc/tz-bot/scripts/renewal_list" ] ; then
         touch /etc/tz-bot/scripts/renewal_list
         chmod 600 /etc/tz-bot/scripts/renewal_list
     fi
-
     if ! [ -e "/etc/tz-bot/scripts/renewal_hook.sh" ] ; then
         touch /etc/tz-bot/scripts/renewal_hook.sh
         chmod +x /etc/tz-bot/scripts/renewal_hook.sh
     fi
-    
     if ! [ -e "/etc/tz-bot/scripts/renewal.sh" ] ; then
         sudo echo "sudo echo '#!/bin/bash' > /etc/tz-bot/scripts/renew_temp.sh" > /etc/tz-bot/scripts/renewal.sh
         sudo echo "sudo echo '. /etc/tz-bot/scripts/.azure_credentials' >> /etc/tz-bot/scripts/renew_temp.sh" >> /etc/tz-bot/scripts/renewal.sh
@@ -241,10 +218,8 @@ function renewal_management() {
                             echo "Crontab entry removed, since no renewals are left in the script."
                             sudo rm /etc/tz-bot/scripts/renewal_list
                             sudo rm /etc/tz-bot/scripts/renewal_hook.sh
-                            sudo touch /etc/tz-bot/scripts/renewal_hook.sh
-                            sudo chmod +x /etc/tz-bot/scripts/renewal_hook.sh
-                            sudo touch /etc/tz-bot/scripts/renewal_list
-                            chmod 600 /etc/tz-bot/scripts/renewal_list
+                            sudo touch /etc/tz-bot/scripts/renewal_hook.sh && sudo chmod +x /etc/tz-bot/scripts/renewal_hook.sh
+                            sudo touch /etc/tz-bot/scripts/renewal_list && chmod 600 /etc/tz-bot/scripts/renewal_list
                         fi
                     else
                         echo "Failed to remove renewal from script."
@@ -262,11 +237,9 @@ function renewal_management() {
             echo
             if [[ "$confirm_all_removal" = "y" ]]; then
                 sudo rm /etc/tz-bot/scripts/renewal_hook.sh
-                sudo touch /etc/tz-bot/scripts/renewal_hook.sh
-                sudo chmod +x /etc/tz-bot/scripts/renewal_hook.sh
+                sudo touch /etc/tz-bot/scripts/renewal_hook.sh && sudo chmod +x /etc/tz-bot/scripts/renewal_hook.sh
                 sudo rm /etc/tz-bot/scripts/renewal_list
-                sudo touch /etc/tz-bot/scripts/renewal_list
-                chmod 600 /etc/tz-bot/scripts/renewal_list
+                sudo touch /etc/tz-bot/scripts/renewal_list && chmod 600 /etc/tz-bot/scripts/renewal_list
                 sudo crontab -l | grep -v '/etc/tz-bot/scripts/renewal.sh' | sudo crontab -
                 echo "All renewals have been removed."
                 renewal_management
@@ -300,9 +273,7 @@ function read_credentials() {
     echo 
     read -p "Please enter your domain: " domain
     echo 
-    echo "export eab_kid=\"$eab_kid\"" > /etc/tz-bot/scripts/.user_credentials
-    echo "export eab_hmac=\"$eab_hmac\"" >> /etc/tz-bot/scripts/.user_credentials
-    chmod 600 /etc/tz-bot/scripts/.user_credentials
+    echo "export eab_kid=\"$eab_kid\"" > /etc/tz-bot/scripts/.user_credentials && echo "export eab_hmac=\"$eab_hmac\"" >> /etc/tz-bot/scripts/.user_credentials && chmod 600 /etc/tz-bot/scripts/.user_credentials
 }
 function dns_full() {
     echo -e "\nWhich DNS provider would you like to use?\n1. Azure DNS\n2. AWS/Route 53\n3. Cloudflare\n4. Domeneshop\n5. infoblox"
@@ -323,9 +294,7 @@ function dns_full() {
             read -p "Please enter your Azure Client Secret: " azure_client_secret && echo "export AZURE_CLIENT_SECRET=\"$azure_client_secret\"" >> /etc/tz-bot/scripts/.azure_credentials
             read -p "Please enter your Azure Tenant ID: " azure_tenant_id && echo "export AZURE_TENANT_ID=\"$azure_tenant_id\"" >> /etc/tz-bot/scripts/.azure_credentials
             read -p "Please enter your Azure Subscription ID: " azure_subscription_id && echo "export AZURE_SUBSCRIPTION_ID=\"$azure_subscription_id\"" >> /etc/tz-bot/scripts/.azure_credentials
-            echo "export AZURE_ENVIRONMENT=\"public\"" >> /etc/tz-bot/scripts/.azure_credentials
-            chmod 600 /etc/tz-bot/scripts/.azure_credentials
-            . /etc/tz-bot/scripts/.azure_credentials
+            echo "export AZURE_ENVIRONMENT=\"public\"" >> /etc/tz-bot/scripts/.azure_credentials && chmod 600 /etc/tz-bot/scripts/.azure_credentials && . /etc/tz-bot/scripts/.azure_credentials
             ;;
         2)
             val_var="--dns route53"
@@ -340,8 +309,7 @@ function dns_full() {
             read -p "Please enter your AWS Access Key ID: " aws_access_key_id && echo "export AWS_ACCESS_KEY_ID=\"$aws_access_key_id\"" > /etc/tz-bot/scripts/.aws_credentials
             read -p "Please enter your AWS Secret Access Key: " aws_secret_access_key && echo "export AWS_SECRET_ACCESS_KEY=\"$aws_secret_access_key\"" >> /etc/tz-bot/scripts/.aws_credentials
             read -p "Please enter your AWS Region: " aws_region && echo "export AWS_REGION=\"$aws_region\"" >> /etc/tz-bot/scripts/.aws_credentials
-            chmod 600 /etc/tz-bot/scripts/.aws_credentials
-            . /etc/tz-bot/scripts/.aws_credentials
+            chmod 600 /etc/tz-bot/scripts/.aws_credentials && . /etc/tz-bot/scripts/.aws_credentials
             ;;
         3)
             val_var="--dns cloudflare"
@@ -355,8 +323,7 @@ function dns_full() {
             fi
             read -p "Please enter your Cloudflare account email: " cloudflare_email && echo "export CLOUDFLARE_EMAIL=\"$cloudflare_email\"" > /etc/tz-bot/scripts/.cloudflare_credentials
             read -p "Please enter your Cloudflare API Key: " cloudflare_api_key && echo "export CLOUDFLARE_API_KEY=\"$cloudflare_api_key\"" >> /etc/tz-bot/scripts/.cloudflare_credentials
-            chmod 600 /etc/tz-bot/scripts/.cloudflare_credentials
-            . /etc/tz-bot/scripts/.cloudflare_credentials
+            chmod 600 /etc/tz-bot/scripts/.cloudflare_credentials && . /etc/tz-bot/scripts/.cloudflare_credentials
             ;;
         4)
             val_var="--dns domeneshop"
@@ -370,8 +337,7 @@ function dns_full() {
             fi
             read -p "Please enter your Domeneshop API Token: " domeneshop_api_token && echo "export DOMENESHOP_API_TOKEN=\"$domeneshop_api_token\"" > /etc/tz-bot/scripts/.domeneshop_credentials
             read -p "Please enter your Domeneshop API Secret: " domeneshop_api_secret && echo "export DOMENESHOP_API_SECRET=\"$domeneshop_api_secret\"" >> /etc/tz-bot/scripts/.domeneshop_credentials
-            chmod 600 /etc/tz-bot/scripts/.domeneshop_credentials
-            . /etc/tz-bot/scripts/.domeneshop_credentials
+            chmod 600 /etc/tz-bot/scripts/.domeneshop_credentials && . /etc/tz-bot/scripts/.domeneshop_credentials
             ;;
         5)
             val_var="--dns infoblox"
@@ -386,8 +352,7 @@ function dns_full() {
             read -p "Please enter your Infoblox username: " infoblox_username && echo "export INFOBLOX_USERNAME=\"$infoblox_username\"" > /etc/tz-bot/scripts/.infoblox_credentials
             read -p "Please enter your Infoblox password: " infoblox_password && echo "export INFOBLOX_PASSWORD=\"$infoblox_password\"" >> /etc/tz-bot/scripts/.infoblox_credentials
             read -p "Please enter your Infoblox host: " infoblox_host && echo "export INFOBLOX_HOST=\"$infoblox_host\"" >> /etc/tz-bot/scripts/.infoblox_credentials
-            chmod 600 /etc/tz-bot/scripts/.infoblox_credentials
-            . /etc/tz-bot/scripts/.infoblox_credentials
+            chmod 600 /etc/tz-bot/scripts/.infoblox_credentials && . /etc/tz-bot/scripts/.infoblox_credentials
             ;;
         *)
             echo "Invalid choice. Exiting."
@@ -396,10 +361,7 @@ function dns_full() {
     esac
 }
 function uninstall() {
-    echo ""
-    echo "Welcome to the TZ-Bot and Lego uninstaller."
-    echo "This will uninstall TZ-Bot and Lego from your system."
-    echo "It will also remove all certificates from /etc/tz-bot/certs/ and all scripts from /etc/tz-bot/scripts/."
+    echo -e "\nWelcome to the TZ-Bot and Lego uninstaller.\nThis will uninstall TZ-Bot and Lego from your system.\nIt will also remove all certificates from /etc/tz-bot/certs/ and all scripts from /etc/tz-bot/scripts/"
     read -n 1 -p "Are you sure you want to proceed? (y/n): " confirm_uninstall
     echo
     if [[ "$confirm_uninstall" == "y" ]]; then
@@ -502,8 +464,7 @@ function ordering() {
     if sudo $lego_var $registration $val_var $path_var $eab $domain_var; then
         cronjob
     else
-        echo -e "\nThere was a problem with the certificate request. Please check your credentials and domain validation."
-        echo "You can also contact TRUSTZONE support at support@trustzone.com"
+        echo -e "\nThere was a problem with the certificate request. Please check your credentials and domain validation." && echo "You can also contact TRUSTZONE support at support@trustzone.com"
         exit
     fi
     if [[ $renewal = yes ]]; then
@@ -511,8 +472,7 @@ function ordering() {
         if sudo grep -qF -- "--domains $domain" "/etc/tz-bot/scripts/renewal_list"; then
             echo "Renewal for $domain already exists in renewal list. Skipping addition."
         else
-            echo "Updating renewal list at: /etc/tz-bot/scripts/renewal_list"
-            echo "sudo $lego_var $registration $val_var $path_var --eab $domain_renew_var" >> /etc/tz-bot/scripts/renewal_list
+            echo "Updating renewal list at: /etc/tz-bot/scripts/renewal_list" && echo "sudo $lego_var $registration $val_var $path_var --eab $domain_renew_var" >> /etc/tz-bot/scripts/renewal_list
         fi
         if [[ "$automatic_restart" == "yes" ]]; then
             auto_reload
@@ -527,12 +487,10 @@ function new_cert() {
     #read -t 0.01 -n 10000 discard    
     case $server_type in
         1)
-            val_var="--apache" && server="apache2"
-            echo -e "\nApache selected"
+            val_var="--apache" && server="apache2" && echo -e "\nApache selected"
             ;;
         2)
-            val_var="--nginx" && server="nginx"
-            echo -e "\nNginx selected"
+            val_var="--nginx" && server="nginx" && echo -e "\nNginx selected"
             ;;
         *)
             echo "Invalid choice, exiting."
@@ -542,16 +500,13 @@ function new_cert() {
 
     case $validation_choice in
         1)
-            lego_var="lego" && val_var="--dns manual"
-            echo -e "MODE: Pre-validated\n" && read_credentials
+            lego_var="lego" && val_var="--dns manual" && echo -e "MODE: Pre-validated\n" && read_credentials
             ;;
         2)
-            lego_var="-E lego"
-            echo -e "MODE: DNS\n" && read_credentials && dns_full
+            lego_var="-E lego" && echo -e "MODE: DNS\n" && read_credentials && dns_full
             ;;
         3)
-            lego_var="lego" && val_var="--http --http.webroot /var/www/html/"
-            echo -e "MODE: HTTP Validation\n" && read_credentials
+            lego_var="lego" && val_var="--http --http.webroot /var/www/html/" && echo -e "MODE: HTTP Validation\n" && read_credentials
             ;;
         *)
             echo "Invalid choice. Exiting."
@@ -564,11 +519,7 @@ function new_cert() {
     fi
     . /etc/tz-bot/scripts/.ca
     registration="--server $selected_ca --email test123@test.com -a"
-
-    #eab var
     eab="--eab --kid "${eab_kid:?}" --hmac "${eab_hmac:?}""
-
-    #domains
     if [[ "$domain" == "*."* ]]; then
         domain_non_wc="${domain#*.}"
         domain_var="--domains "${domain:?}" --domains "${domain_non_wc:?}" --key-type rsa2048 run"
@@ -581,18 +532,15 @@ function new_cert() {
     read -n 1 -p "Do you want to specify where the certificate is saved? (y/n): " custom_path_choice
     if [[ "$custom_path_choice" == "y" ]]; then
         read -p "Please enter the full path to save the certificates (e.g., /etc/tz-bot/certs): " custom_path 
-        echo -e "\nCustom path selected: $custom_path" && echo "path=$custom_path" > /etc/tz-bot/scripts/storage
-        . /etc/tz-bot/scripts/storage
+        echo -e "\nCustom path selected: $custom_path" && echo "path=$custom_path" > /etc/tz-bot/scripts/storage && . /etc/tz-bot/scripts/storage
         path_var="--path $path"
     else
-        echo -e "\nUsing default path for certificate storage: /etc/tz-bot/certs/" && echo "path=/etc/tz-bot/certs" > /etc/tz-bot/scripts/storage
-        . /etc/tz-bot/scripts/storage
+        echo -e "\nUsing default path for certificate storage: /etc/tz-bot/certs/" && echo "path=/etc/tz-bot/certs" > /etc/tz-bot/scripts/storage && . /etc/tz-bot/scripts/storage
         path_var="--path $path"
     fi
-
     ordering
     start_prompt
 }
-# Start
+
 upkeep
 start_prompt
