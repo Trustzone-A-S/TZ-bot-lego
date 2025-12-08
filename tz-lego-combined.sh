dc19a3d5759b2754e6f75a5c6376fa4dc49f45aa
janniktaulan <janniktn1999@hotmail.com>
2025-12-08 10:37:42 +0100

    ver

--- tz-lego-combined.sh
+++ tz-lego-combined.sh
@@ -62,7 +62,7 @@ function auto_reload() {
     fi
 }
 function upkeep() {
-    local_version="1.4.2"
+    local_version="1.4.3"
     if [ "$(id -u)" -ne 0 ]; then
         echo 'This script must be run by root' >&2
         exit 1
