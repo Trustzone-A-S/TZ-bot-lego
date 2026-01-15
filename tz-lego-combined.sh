f2adeb78403e4f4ff80f703e35b7ab75c2fd60bf
janniktaulan <janniktn1999@hotmail.com>
2026-01-15 15:04:35 +0100

    typo fix

--- tz-lego-combined.sh
+++ tz-lego-combined.sh
@@ -229,7 +229,7 @@ function yn_prompt() {
 }
 function renewal_management() {
     echo -e "\nRenewal management:\n1. List renewals\n2. Run renewal script\n3. Forcefully run renewal script\n4. Forcefully run a specific renewal\n5. Remove a cronjob renewal\n6. Remove all cronjob renewals\n7. Back"
-    read -p "Enter choice [1-5]: " renewal_choice
+    read -p "Enter choice [1-7]: " renewal_choice
     echo
     case $renewal_choice in
         1)
